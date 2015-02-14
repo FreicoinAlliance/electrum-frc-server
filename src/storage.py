9ef3e654e60b0eae9d24705a4ecd7a0bc8d05ef0
Kefkius <kefkius@mail.com>
2015-02-14 15:23:55 -0500

    FRC - genesis block

--- src/storage.py
+++ src/storage.py
@@ -8,7 +8,7 @@ from processor import print_log, logger
 from utils import bc_address_to_hash_160, hash_160_to_pubkey_address, hex_to_int, int_to_hex, Hash
 
 global GENESIS_HASH
-GENESIS_HASH = '000000000019d6689c085ae165831e934ff763ae46a2a6c172b3f1b60a8ce26f'
+GENESIS_HASH = '000000005b1e3d23ecfd2dd4a6e1a35238aa0392c0a8528c40df52376d7efe2c'
 
 """
 Patricia tree for hashing unspents
